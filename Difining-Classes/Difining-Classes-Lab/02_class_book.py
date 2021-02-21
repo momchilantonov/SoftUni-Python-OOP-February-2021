5279dd3e1c9dfec89792d991c4b80519adf84fda
momchilantonov <eng.antonov@gmail.com>
2021-02-21 02:19:57 +0200

    Insert a Solution with @dataclass

--- Difining-Classes/Difining-Classes-Lab/02_class_book.py
+++ Difining-Classes/Difining-Classes-Lab/02_class_book.py
@@ -1,3 +1,6 @@
+# from dataclasses import dataclass
+
+
 class Book:
     def __init__(self, name, author, pages):
         self.name = name
@@ -5,7 +8,19 @@ class Book:
         self.pages = pages
 
 
+# @dataclass
+# class Book2:
+#     name: str
+#     author: str
+#     pages: int
+
+
 book = Book("My Book", "Me", 200)
 print(book.name)
 print(book.author)
 print(book.pages)
+
+# book2 = Book2("My Book", "Me", 200)
+# print(book2.name)
+# print(book2.author)
+# print(book2.pages)
