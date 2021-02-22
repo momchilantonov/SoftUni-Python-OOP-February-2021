0250196a7f323b2750adbdf40e3d896ad038d863
momchilantonov <eng.antonov@gmail.com>
2021-02-23 00:06:16 +0200

    Problem 5

---
+++ Difining-Classes/Difining-Classes-Exercise/programmer_05/project/programmer.py
@@ -0,0 +1,30 @@
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if language == self.language:
+            self.skills += skills_earned
+            return f"{self.name} watched {course_name}"
+        return f"{self.name} does not know {language}"
+
+    def change_language(self, new_language, skills_needed):
+        if self.skills >= skills_needed:
+            if not self.language == new_language:
+                previous_language = self.language
+                self.language = new_language
+                return f"{self.name} switched from {previous_language} to {new_language}"
+            return f"{self.name} already knows {new_language}"
+        needed_skills = skills_needed-self.skills
+        return f"{self.name} needs {needed_skills} more skills"
+
+
+programmer = Programmer("John", "Java", 50)
+print(programmer.watch_course("Python Masterclass", "Python", 84))
+print(programmer.change_language("Java", 30))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Java: zero to hero", "Java", 50))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Python Masterclass", "Python", 84))
