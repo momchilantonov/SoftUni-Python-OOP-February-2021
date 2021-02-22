2b1212b8ddf53aeaddd597af1417ca64532c38eb
momchilantonov <eng.antonov@gmail.com>
2021-02-22 16:37:53 +0200

    Problem 8

---
+++ Difining-Classes/Difining-Classes-Exercise/spoopify_08/project/album.py
@@ -0,0 +1,45 @@
+# from song import Song
+
+
+class Album:
+    def __init__(self, name, songs=None):
+        self.name = name
+        if songs is None:
+            songs = []
+        if not isinstance(songs, list):
+            self.songs = []
+            self.songs.append(songs)
+        else:
+            self.songs = songs
+        self.published = False
+
+    def add_song(self, song):
+        if song.single:
+            return f"Cannot add {song.name}. It's a single"
+        if self.published:
+            return "Cannot add songs. Album is published."
+        if song in self.songs:
+            return "Song is already in the album."
+        self.songs.append(song)
+        return f"Song {song.name} has been added to the album {self.name}."
+
+    def remove_song(self, song_name):
+        if song_name not in [song.name for song in self.songs]:
+            return "Song is not in the album."
+        if self.published:
+            return "Cannot remove songs. Album is published."
+        song = [s for s in self.songs if s.name == song_name][0]
+        self.songs.remove(song)
+        return f"Removed song {song.name} from album {self.name}."
+
+    def publish(self):
+        if self.published:
+            return f"Album {self.name} is already published."
+        self.published = True
+        return f"Album {self.name} has been published."
+
+    def details(self):
+        result = f"Album {self.name}\n"
+        for song in self.songs:
+            result += f"== {song.get_info()}\n"
+        return result
---
+++ Difining-Classes/Difining-Classes-Exercise/spoopify_08/project/band.py
@@ -0,0 +1,43 @@
+# from album import Album
+# from song import Song
+
+
+class Band:
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album):
+        if album in self.albums:
+            return f"Band {self.name} already has {album.name} in their library."
+        self.albums.append(album)
+        return f"Band {self.name} has added their newest album {album.name}."
+
+    def remove_album(self, album_name):
+        album_list = [a for a in self.albums if a.name == album_name]
+        if album_list:
+            album = album_list[0]
+            if album.published:
+                return "Album has been published. It cannot be removed."
+            self.albums.remove(album)
+            return f"Album {album.name} has been removed."
+        return f"Album {album_name} is not found."
+
+    def details(self):
+        result = f"Band {self.name}\n"
+        for album in self.albums:
+            result += f"{album.details()}\n"
+        return result
+
+
+# song = Song("Running in the 90s", 3.45, False)
+# print(song.get_info())
+# album = Album("Initial D", song)
+# second_song = Song("Around the World", 2.34, False)
+# print(album.add_song(second_song))
+# print(album.details())
+# print(album.publish())
+# band = Band("Manuel")
+# print(band.add_album(album))
+# print(band.remove_album("Initial D"))
+# print(band.details())
