c66252ae6d437092d83168b624998aeb28a126eb
momchilantonov <eng.antonov@gmail.com>
2021-03-01 23:00:14 +0200

    Problem 2

---
+++ Encapsulation/Encapsulation-Lab/02_email_validator.py
@@ -0,0 +1,30 @@
+import re
+
+
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __validate_name(self, name):
+        return name and self.min_length <= len(name)
+
+    def __validate_mail(self, mail):
+        return mail in self.mails
+
+    def __validate_domain(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        name, mail, domain = re.split(r"[@.]", email)
+        return self.__validate_name(name) and self.__validate_mail(mail) and self.__validate_domain(domain)
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
