fe05bb451b5000913eea0e8c84d98265a07e6b0a
momchilantonov <eng.antonov@gmail.com>
2021-03-01 22:59:00 +0200

    Add some code (props) Problem 1

--- Encapsulation/Encapsulation-Lab/01_person.py
+++ Encapsulation/Encapsulation-Lab/01_person.py
@@ -10,6 +10,48 @@ class Person:
         return self.__age
 
 
+# class Person:
+#     def __init__(self, name, age):
+#         self.__name = name
+#         self.__age = age
+#
+#     @property
+#     def name(self):
+#         return self.__name
+#
+#     @name.setter
+#     def name(self, value):
+#         self.__name = value
+#
+#     @property
+#     def age(self):
+#         return self.__age
+#
+#     @age.setter
+#     def age(self, value):
+#         self.__age = value
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     @property
+#     def name(self):
+#         return self.__name
+#
+#     @name.setter
+#     def name(self, value):
+#         if not value or not isinstance(value, str):
+#             raise ValueError("Name must be a non-empty string")
+#         self.__name = value
+
+
 person = Person("George", 32)
 print(person.get_name())
 print(person.get_age())
+
+# person = Person("George", 32)
+# print(person.name)
+# print(person.age)
