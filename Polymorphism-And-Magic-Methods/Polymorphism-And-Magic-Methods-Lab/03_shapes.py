e24caa27a0bd898e7190270a4d91d0c5cbec7baf
momchilantonov <eng.antonov@gmail.com>
2021-03-08 16:59:56 +0200

    Problem 3

---
+++ Polymorphism-And-Magic-Methods/Polymorphism-And-Magic-Methods-Lab/03_shapes.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return pi * self.radius ** 2
+
+    def calculate_perimeter(self):
+        return 2 * pi * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def calculate_area(self):
+        return self.height * self.width
+
+    def calculate_perimeter(self):
+        return  2 * (self.height+self.width)
+
+
+circle = Circle(5)
+print(circle.calculate_area())
+print(circle.calculate_perimeter())
+
+rectangle = Rectangle(10, 20)
+print(rectangle.calculate_area())
+print(rectangle.calculate_perimeter())
