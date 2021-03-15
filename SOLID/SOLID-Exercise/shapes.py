b4239e714e828d350a9c53ae05beb5a7878429f0
momchilantonov <eng.antonov@gmail.com>
2021-03-15 14:54:49 +0200

    Problem shapes

---
+++ SOLID/SOLID-Exercise/shapes.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+
+
+class Triangle(Shape):
+    def __init__(self, side, h):
+        self.side = side
+        self.h = h
+
+    def calculate_area(self):
+        return self.side * self.h / 2
+
+
+class AreaCalculator:
+    def __init__(self, shapes):
+        assert isinstance(shapes, list), "`shapes` should be of type `list`."
+        self.shapes = shapes
+
+    @property
+    def total_area(self):
+        total = 0
+        for shape in self.shapes:
+            total += shape.calculate_area()
+
+        return total
+
+
+shapes = [Rectangle(1, 6), Triangle(2, 3)]
+calculator = AreaCalculator(shapes)
+
+print("The total area is: ", calculator.total_area)
