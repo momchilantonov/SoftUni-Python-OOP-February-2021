33c177336cf0aaf189dfeafe345844d4f79ce446
momchilantonov <eng.antonov@gmail.com>
2021-03-16 19:10:02 +0200

    Problem 3

---
+++ Iterators-And-Generators/Iterators-And-Generators-Exercise/03_countdown_iterator.py
@@ -0,0 +1,19 @@
+class countdown_iterator:
+    def __init__(self, count):
+        self.count = count
+        self.counter = self.count
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count >= 0:
+            temp_count = self.count
+            self.count -= 1
+            return temp_count
+        raise StopIteration()
+
+
+iterator = countdown_iterator(10)
+for item in iterator:
+    print(item, end=" ")
